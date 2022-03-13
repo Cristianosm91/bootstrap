ee0c8a7c3934eeb4b234041dc0081cdc50c0507b
Cristianosm91 <csm90csm@hotmail.com>
2022-03-13 14:18:03 -0300

    Projeto final, fim do curso

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//Enable tooltips everywhere
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
+
+//Set the carousel transition interval
+$('.carousel').carousel({
+    interval: 4000
+  })
